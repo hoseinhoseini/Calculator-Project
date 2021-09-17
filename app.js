bb56f919a5bb2f0cae68ec0bc62a7a41c4ffdfc5
HosseinHosseini <hoseinhoseini2171384@gmail.com>
2021-09-17 08:39:58 +0430

    Completed Export Of Result

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-gsap.from('.container', { duration: 2, opacity: 0, stagger: 0.5, y: 500 })
+// gsap.from('.container', { duration: 2, opacity: 0, stagger: 0.5, y: 500 })
 
 const btns = document.querySelectorAll('.num-button')
 const allBtns = document.querySelectorAll('.button')
@@ -7,3 +7,13 @@ const clearBtn = document.querySelector('#clear')
 const total = document.querySelector('#total')
 const btnSpread = [...btns]
 const allBtnSpread = [...allBtns]
+
+btnSpread.forEach((button, i) => {
+    button.addEventListener('click', () => {
+        if (resultBox.innerHTML == '0') {
+            resultBox.innerHTML = ''
+        }
+        let value = btns[i].innerHTML
+        resultBox.innerHTML += value
+    })
+})
